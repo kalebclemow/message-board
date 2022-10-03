e8f2d507c0586cceffe829283e954ddd8a3b6b30
Kaleb Clemow <kc181349@umconnect.umt.edu>
2022-10-03 16:43:53 -0600

    tests commit

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Post
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text='This is a test!')
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, 'This is a test!')
